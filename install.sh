08e419124bbe104b82d6d15c201b31c18c8c47d1
yen <yen.linkwang@nigao.re>
2019-12-03 13:23:28 +0400

    install.sh: create

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# force to install dependencies if some is missing
+composer install
+
+# force drop database
+php bin/console doctrine:database:drop --force
+php bin/console doctrine:database:create
+
+# prepare database schema
+yes | php bin/console doctrine:migrations:migrate
+
+# load data samples
+yes | php bin/console doctrine:fixtures:load
+
+# launch http server
+php bin/console server:stop
+php bin/console server:start
+
+php bin/console cron:start
+
+#launch a debugger server; in your controller use ```dump( $var );``` to obtain a var_dump
+php bin/console server:dump
