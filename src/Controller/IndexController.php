1a2d376b2cffd7d13d734ebeb52ead5b8bd88bb2
yen <yen.linkwang@nigao.re>
2019-12-03 11:10:43 +0400

    Controller/IndexController.php:

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/", name="index")
      */
     public function index()
     {
