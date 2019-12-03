4fa9905792a93c91a9e38fa129ca8dc9942add3d
yen <yen.linkwang@nigao.re>
2019-12-03 13:42:10 +0400

    install script

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 #passage à PHP7.3
 export PATH=/opt/plesk/php/7.3/bin/:$PATH
 
+#remove old migrations
+cd src/Migrations
+rm *.php
+cd ../..
+
 # force to install dependencies if some is missing
 composer install
 
@@ -11,16 +16,24 @@ php bin/console doctrine:database:drop --force
 php bin/console doctrine:database:create
 
 # prepare database schema
+php bin/console make:migration
+
+# replace ALL JSOPN by LONGTEXT
+cd src/Migrations
+#eddy here
+cd ../..
+
+# populate database
 yes | php bin/console doctrine:migrations:migrate
 
 # load data samples
 yes | php bin/console doctrine:fixtures:load
 
-# launch http server
-php bin/console server:stop
-php bin/console server:start
+# php bin/console cron:start
 
-php bin/console cron:start
+# launch http server
+symfony local:server:stop
+symfony local:server:start --no-tls
 
 #launch a debugger server; in your controller use ```dump( $var );``` to obtain a var_dump
-php bin/console server:dump
+#php bin/console server:dump
