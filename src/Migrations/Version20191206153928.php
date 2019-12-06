f25c6b112fdcadee2797861753c1ae416941f83f
yen <yen.linkwang@nigao.re>
2019-12-06 19:40:09 +0400

    Migrations/Version201912061:

--- src/Migrations/Version20191206153928.php
+++ src/Migrations/Version20191206153928.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191206134648 extends AbstractMigration
+final class Version20191206153928 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,10 +22,10 @@ final class Version20191206134648 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE eco_level (id INT AUTO_INCREMENT NOT NULL, level DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(60) NOT NULL, address LONGTEXT NOT NULL, latitude DOUBLE PRECISION NOT NULL, longitude DOUBLE PRECISION NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE photo (id INT AUTO_INCREMENT NOT NULL, host_place_id INT NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_14B784185C4F5E0F (host_place_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE activity (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(60) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE eco_level (id INT AUTO_INCREMENT NOT NULL, level DOUBLE PRECISION NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(60) NOT NULL, address LONGTEXT NOT NULL, latitude DOUBLE PRECISION NOT NULL, longitude DOUBLE PRECISION NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE photo (id INT AUTO_INCREMENT NOT NULL, host_place_id INT NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_14B784185C4F5E0F (host_place_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE host_place (id INT AUTO_INCREMENT NOT NULL, owner_id INT NOT NULL, activity_id INT NOT NULL, address LONGTEXT NOT NULL, INDEX IDX_D77F1E3E7E3C61F9 (owner_id), INDEX IDX_D77F1E3E81C06096 (activity_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE photo ADD CONSTRAINT FK_14B784185C4F5E0F FOREIGN KEY (host_place_id) REFERENCES host_place (id)');
         $this->addSql('ALTER TABLE host_place ADD CONSTRAINT FK_D77F1E3E7E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id)');
@@ -37,13 +37,13 @@ final class Version20191206134648 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE host_place DROP FOREIGN KEY FK_D77F1E3E7E3C61F9');
         $this->addSql('ALTER TABLE host_place DROP FOREIGN KEY FK_D77F1E3E81C06096');
+        $this->addSql('ALTER TABLE host_place DROP FOREIGN KEY FK_D77F1E3E7E3C61F9');
         $this->addSql('ALTER TABLE photo DROP FOREIGN KEY FK_14B784185C4F5E0F');
+        $this->addSql('DROP TABLE activity');
         $this->addSql('DROP TABLE eco_level');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE photo');
-        $this->addSql('DROP TABLE activity');
         $this->addSql('DROP TABLE host_place');
     }
 }
