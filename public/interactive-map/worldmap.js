03d281821a8a439aee139afeb3b64c81e63f7885
Minijinski <36927054+Minijinski@users.noreply.github.com>
2019-12-07 02:00:59 +0300

    Update worldmap.js

--- public/interactive-map/worldmap.js
+++ public/interactive-map/worldmap.js
@@ -58,6 +58,10 @@
 		var location_set=paper.set();
 		
 		//create locations
+		console.log("Parsed: ", JSON.parse(localStorage.getItem("Locations")))
+		if (localStorage.getItem("Locations") !== null)
+			console.log("ATTRIBUTING")
+			locations = localStorage.getItem("Locations")	
 		for (var location in locations){
 			var loc=locations[location];
 			console.log("NAME: ", loc.name);
