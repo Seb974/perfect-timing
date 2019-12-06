59b37df7acc57593f9cabe3e6b893d7750cb2e13
Sébastien Maillot <sebastienmaillot@MacBook-Pro-de-Sebastien.local>
2019-12-06 18:01:30 +0300

    Constraints added to the forms

--- src/Form/EcoLevelType.php
+++ src/Form/EcoLevelType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\EcoLevel;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,10 @@ class EcoLevelType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('level')
+            ->add('level', NumberType::class, [
+                'mapped' => false,
+                'required' => false,
+            ])
         ;
     }
 
