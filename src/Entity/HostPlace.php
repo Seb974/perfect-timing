3d66a4b0a81c72021070365581641887c6e5afab
Sébastien Maillot <sebastienmaillot@MacBook-Pro-de-Sebastien.local>
2019-12-06 16:33:42 +0300

    Entity Photo created

--- src/Entity/HostPlace.php
+++ src/Entity/HostPlace.php
@@ -31,10 +31,16 @@ class HostPlace
      */
     private $owner;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Photo", mappedBy="hostPlace", orphanRemoval=true)
+     */
+    private $photos;
+
     public function __construct()
     {
         $this->candidates = new ArrayCollection();
         $this->selected = new ArrayCollection();
+        $this->photos = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -62,6 +68,37 @@ class HostPlace
     public function setOwner(?User $owner): self
     {
         $this->owner = $owner;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Photo[]
+     */
+    public function getPhotos(): Collection
+    {
+        return $this->photos;
+    }
+
+    public function addPhoto(Photo $photo): self
+    {
+        if (!$this->photos->contains($photo)) {
+            $this->photos[] = $photo;
+            $photo->setHostPlace($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhoto(Photo $photo): self
+    {
+        if ($this->photos->contains($photo)) {
+            $this->photos->removeElement($photo);
+            // set the owning side to null (unless already changed)
+            if ($photo->getHostPlace() === $this) {
+                $photo->setHostPlace(null);
+            }
+        }
 
         return $this;
     }
