ec40a65d8f6165f856a89e98327ddce72cb56842
Sébastien Maillot <sebastienmaillot@MacBook-Pro-de-Sebastien.local>
2019-12-07 04:44:45 +0300

    crud Transaction done

---
+++ src/Form/TransactionType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Transaction;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TransactionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('username')
+            ->add('motivation')
+            ->add('dateSelected')
+            ->add('state')
+            ->add('relation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Transaction::class,
+        ]);
+    }
+}
